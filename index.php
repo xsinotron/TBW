c8b272413524da2d3ecf4b8a4f71fa764265e523
Alexis Collin <contact@alexis-collin.fr>
2014-03-24 23:01:12 +0100

    première grille

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+$grid = array(
+    4,
+    6
+);
+$robot = array(
+    0,
+    0
+);
+function makeGrid ($gridsize=array(), $item=array()) {
+    $x    = $gridsize[0];
+    $y    = $gridsize[1];
+    $grid = "<table class='grid'>\n";
+
+    for ($i = 0; $i < $y; $i++) {
+        $grid .= '<tr>';
+        for ($j = 0; $j < $x; $j++) {
+            $isRobot = $item[0] === $j && $item[1] === $i;
+            $content = ($isRobot) ? "<span class='robot'>R</span>" : "";
+            $grid .= "<td>$content</td>";
+        }
+        $grid .= "</tr>\n";
+    }
+
+    $grid .= '</table>';
+    echo $grid;
+}
+?>
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>TBW</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/boilertemplate.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <div class="main"><?php makegrid($grid, $robot); ?>
+        </div>script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,48 @@
+(function (window) {
+    'use strict';
+    var $   = window.$,
+        TBW = function () {
+
+        /**
+         *
+         */
+        function actionKeys (ev) {
+            ev.preventDefault();
+            var td = $(ev.currentTarget);
+            console.log(ev.key, ev.keyCode);
+            // haut
+            if (ev.keyCode === 38) console.log("haut");
+            // bas
+            if (ev.keyCode === 40) console.log("bas");
+            // gauche
+            if (ev.keyCode === 37) console.log("gauche");
+            // droite
+            if (ev.keyCode === 39) console.log("droite");
+        }
+        /**
+         *
+         */
+        function actionBtn (ev) {
+            ev.preventDefault();
+            var td = $(ev.currentTarget);
+            if (td.find('.robot').length > 0) alert("un robot!");
+            else console.log("personne ici");
+        }
+        /**
+         *
+         */
+        function initBtn () {
+            $(".grid tr td").off().on('click', actionBtn);
+            $("html").off().on('keyup', actionKeys);
+        }
+        /**
+         *
+         */
+        function init () {
+            initBtn();
+        }
+        this.init = init;
+    };
+    var control = new TBW();
+    control.init();
+}(window));
